d3dc92cea22cc52fb9ab243bada2917121c2fdc5
uptobe8 <ia.uptobe@gmail.com>
2026-08-16 00:05:51 +0200

    old

--- server.py
+++
@@ -1,7 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
-import os
-ROOT = Path(__file__).resolve().parent
-os.chdir(ROOT)
-print('Tu Ruta Cádiz: http://127.0.0.1:9100')
-ThreadingHTTPServer(('127.0.0.1',9100), SimpleHTTPRequestHandler).serve_forever()
