2a48d0cb7772d555af1f5e7f92809b7bee603e12
uptobe8 <ia.uptobe@gmail.com>
2026-08-16 01:02:47 +0200

    sss

--- server.py
+++
@@ -1,7 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
-import os
-root=Path(__file__).resolve().parent
-os.chdir(root)
-print('Abre http://localhost:8080')
-ThreadingHTTPServer(('0.0.0.0',8080),SimpleHTTPRequestHandler).serve_forever()
